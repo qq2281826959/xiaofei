3d84d2f52828e9a6aaeed466100df9b3dc2b5050
qzxbg <qzxbg@qq.com>
2020-02-17 11:19:29 +0800

    Create index.vue

---
+++ src/page/Home/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class='home'>
+		<Header></Header>
+		<router-view></router-view>
+	</div>
+</template>
+
+<script>
+	
+	import Header from '@/components/Header'
+	
+	export default{
+		name:'Home',
+		components:{
+			Header
+		}
+	}
+</script>
+
+<style scoped>
+	.home{
+		display:flex;
+		justify-content: flex-start;
+		flex-direction: column;
+		align-items: center;
+		width: 100%;
+	}
+</style>
